dc6db1544ae92bb26f50f330295b0b68e6423e73
Sébastien Randriamanana <sebastienrandriamanana3@gmail.com>
2025-08-14 13:09:11 +0400

    third commit

--- api/foods.js
+++ api/foods.js
@@ -1,6 +1,17 @@
 import fs from "fs";
+import path from "path";
+import { fileURLToPath } from 'url';
+
+// Récupérer le dossier actuel de ce fichier
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default function handler(req, res) {
-  const db = JSON.parse(fs.readFileSync("db.json", "utf-8"));
+  // Construire le chemin vers db.json à la racine
+  const dbPath = path.join(__dirname, "../db.json");
+  
+  // Lire le fichier
+  const db = JSON.parse(fs.readFileSync(dbPath, "utf-8"));
+
   res.status(200).json(db.foods);
 }
